0d4ebb3f2a166f35eaeb32a6963b504b28e7ca8f
leon <coderleonkang@gmail.com>
2023-02-01 16:21:35 +0800

    Add pixi support

--- src/pixiDisplay.js
+++ src/pixiDisplay.js
@@ -1,41 +1,46 @@
-const { Live2DModel } = require("pixi-live2d-display");
+const { Live2DModel, Cubism2ModelSettings } = require("pixi-live2d-display");
 const PIXI = require("pixi.js");
 const path = require("path");
 const fs = require("fs");
+const { Ticker, TickerPlugin } = require("@pixi/ticker")
+const { InteractionManager } = require('@pixi/interaction');
 
 const datasetRoot = "dataset"; // Root of dataset directory
 const outputRoot = "output"; // Root of output directory
 const blacklistPath = path.join(outputRoot, "blacklist.txt"); // Blacklist path
 const baseResolution = 1024;
 
-var thisRef = this;
-
-// require("src/live2d/live2dcubismcore.min.js");
+const thisRef = this;
 
 window.onerror = function (msg, url, line, col, error) {
     const err = "file:" + url + "<br>line:" + line + " " + msg;
-    l2dError(err);
+    console.log(err);
 };
 
 window.PIXI = PIXI;
 
-function pixiViewer() {
+async function pixiViewer() {
     this.platform = window.navigator.platform.toLowerCase();
     this.app = new PIXI.Application({
-        view: document.getElementById('glcanvas'),
+        view: document.getElementById('canvas'),
+        width: 1024,
+        height: 1024,
+        autoStart: true,
     });
-    pixiViewer.init();
-}
 
-pixiViewer.init = function () {
-    loadPixiModel();
+    PIXI.extensions.add(InteractionManager);
+    Live2DModel.registerTicker(Ticker);
+    this.app.renderer.backgroundColor = 0xFF5733
 
+    const model = await loadPixiModel();
+
+    this.app.stage.addChild(model);
 }
 
 function walkdir(dir, callback) {
     const files = fs.readdirSync(dir);
     files.forEach((file) => {
-        var filepath = path.join(dir, file);
+        const filepath = path.join(dir, file);
         const stats = fs.statSync(filepath);
         if (stats.isDirectory()) {
             walkdir(filepath, callback);
@@ -45,16 +50,15 @@ function walkdir(dir, callback) {
     });
 }
 
-async function loadPixiModel() {
+function loadPixiModel() {
     let filelist = [];
-    walkdir(datasetRoot, function (filepath) {
-        if (filepath.endsWith(".model.json")) {
+    walkdir("dataset", function (filepath) {
+        if (filepath.endsWith(".model3.json")) {
             filelist.push(filepath);
         }
     });
     console.log("pixi file path: " + filelist);
-    if (this.usingPixi) {
-        const model = await Live2DModel.from(filelist[filelist.length - 1]);
-        this.app.stage.addChild(model);
-    }
+    const last = filelist[filelist.length - 1];
+    console.log(last);
+    return Live2DModel.from(last);
 }
